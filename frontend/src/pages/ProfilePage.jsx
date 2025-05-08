70dffc4ff77a7262d4ac907d09484f7acf16be7b
Preetam <preetamdas248@gmail.com>
2025-05-09 01:25:17 +0530

    add cross button in setting page and profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore.js";
 import { Camera, Mail, User } from "lucide-react";
+import { X } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function ProfilePage() {
   const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
   const [selectedImg, setSelectedImg] = useState(null);
-
+  const navigate = useNavigate();
   const handleImageUpload = (e) => {
     e.preventDefault();
     const image = e.target.files[0];
@@ -34,9 +36,14 @@ function ProfilePage() {
     <div className="h-screen pt-20">
       <div className="max-w-2xl mx-auto p-4 py-8">
         <div className="bg-base-300 rounded-xl p-6 space-y-8">
-          <div className="text-center">
+          <div className="text-center relative">
             <h1 className="text-2xl font-semibold ">Profile</h1>
             <p className="mt-2">Your profile information</p>
+            <button
+              className="absolute right-0 top-0"
+              onClick={() => navigate(-1)}>
+              <X />
+            </button>
           </div>
 
           {/* avatar upload section */}
--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,7 +1,8 @@
 import { THEMES } from "../constants";
 import { useThemeStore } from "../store/useThemeStore";
 import { Send } from "lucide-react";
-
+import { X } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 const PREVIEW_MESSAGES = [
   { id: 1, content: "Hey! How's it going?", isSent: false },
   {
@@ -13,15 +14,20 @@ const PREVIEW_MESSAGES = [
 
 const SettingsPage = () => {
   const { theme, setTheme } = useThemeStore();
-
+  const navigate = useNavigate();
   return (
     <div className="h-screen container mx-auto px-4 pt-20 max-w-5xl">
       <div className="space-y-6">
-        <div className="flex flex-col gap-1">
-          <h2 className="text-lg font-semibold">Theme</h2>
-          <p className="text-sm text-base-content/70">
-            Choose a theme for your chat interface
-          </p>
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-1">
+            <h2 className="text-lg font-semibold">Theme</h2>
+            <p className="text-sm text-base-content/70">
+              Choose a theme for your chat interface
+            </p>
+          </div>
+          <button onClick={()=>navigate(-1)}>
+            <X />
+          </button>
         </div>
 
         <div className="grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 gap-2">
